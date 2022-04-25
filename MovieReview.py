11a1c871a068d90aa793e46d9bcd4b69c7e60eb2
ketan1729 <103006434+ketan1729@users.noreply.github.com>
2022-04-25 18:37:38 -0400

    Created Bins

--- MovieReview.py
+++ MovieReview.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import math
+import csv
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics import mean_squared_error
@@ -62,8 +63,8 @@ def get_ratings():
     pred = []
     c = 1
     for index, row in test_data.iterrows():
-        #print("For iter :", c)
-        #c += 1
+        # print("For iter :", c)
+        # c += 1
         user_id = int(row['userId'])
         movie_id = row['movieId']
         res = predict_rating(user_id, movie_id)
@@ -98,7 +99,6 @@ def calculate_rmse():
 
 
 def get_predicted_ratings_dict(train_data, test_data):
-    res_dict = {}
     movies_train = train_data['movieId'].unique().tolist()
     movies_test = test_data['movieId'].unique().tolist()
     movies = []
@@ -107,14 +107,40 @@ def get_predicted_ratings_dict(train_data, test_data):
     movies = set(movies)
 
     users = train_data['userId'].unique()
+    user_bins = []
+    bin_size = 10
+    print("Number of bins = ", len(users) / bin_size)
+    curr_bin = []
     for user in users:
-        curr_rec = []
-        movies_to_predict = [x for x in movies if x not in train_data[train_data['userId'] == user]['movieId'].unique()]
-        for movie in movies_to_predict:
-            pred_mov_rating = predict_rating(user, movie)
-            curr_rec.append((movie, pred_mov_rating))
-        res_dict[user] = sorted(curr_rec, key=lambda x: x[1])
-    return res_dict
+        if len(curr_bin) == bin_size:
+            user_bins.append(curr_bin)
+            curr_bin = []
+        curr_bin.append(user)
+
+    for user_bin_idx in range(len(user_bins)):
+        user_bin = user_bins[user_bin_idx]
+        f_name = "PredictedRatings\\Bin" + str(61)+".csv"
+        outcsv = open(f_name, 'w', newline='')
+        writer = csv.writer(outcsv)
+        writer.writerow(["user", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9", "R10"])
+
+        for user in user_bin:
+            curr_rec = []
+            res_row = [user]
+            movies_to_predict = [x for x in movies if
+                                 x not in train_data[train_data['userId'] == user]['movieId'].unique()]
+            for movie in movies_to_predict:
+                pred_mov_rating = predict_rating(user, movie)
+                curr_rec.append((movie, pred_mov_rating))
+            curr_rec_sorted_list = sorted(curr_rec, key=lambda x: x[1])
+            res_row.extend([i[0] for i in curr_rec_sorted_list[:10]])
+            writer.writerow(res_row)
+        outcsv.close()
+
+
+def create_pred_ratings_dict():
+    res_dict = {}
+    
 
 
 def get_recom_metrics(test_data, pred_ratings_dict):
@@ -128,7 +154,7 @@ def get_recom_metrics(test_data, pred_ratings_dict):
             if pred_ratings[i][0] in test_data[test_data['userId'] == user]['movieId'].unique():
                 user_precision += 1
                 recall += 1
-        precision += user_precision/10
+        precision += user_precision / 10
         recall += user_recall / len(test_data[test_data['userId'] == user]['movieId'].unique())
 
     recall /= len(pred_ratings_dict)
@@ -142,13 +168,13 @@ if __name__ == '__main__':
     train_data, test_data = get_train_and_test_data()
     x_train, y_train = create_utility_mat()
     sim_mat = get_similarity_matrix()
-    y_pred = get_ratings()
-    #compare()
-    calculate_rmse()
-    ratings_dict = get_predicted_ratings_dict(train_data, test_data)
-    precision, recall, f_measure = get_recom_metrics(test_data, ratings_dict)
-    print('Precision: ', precision)
-    print('Recall: ', recall)
-    print('F Measure: ', f_measure)
+    #y_pred = get_ratings()
+    # compare()
+    #calculate_rmse()
+    #get_predicted_ratings_dict(train_data, test_data)
+    #precision, recall, f_measure = get_recom_metrics(test_data, ratings_dict)
+    #print('Precision: ', precision)
+    #print('Recall: ', recall)
+    #print('F Measure: ', f_measure)
 
     # print(get_top_k_similar_users_rated(train_data, sim_mat, 5, 1, 4))
